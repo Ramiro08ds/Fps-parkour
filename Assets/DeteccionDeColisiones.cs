80d42467c9dc13226add3f9f301dcb102ca5b9cf
Ramiro08ds <Sanchezriverazaied@gmail.com>
2025-05-26 09:06:25 -0300

    26/05
    colisiones terminadas, falta hacer el scoremanager

--- Assets/DeteccionDeColisiones.cs
+++ Assets/DeteccionDeColisiones.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 
 public class DeteccionDeColisiones : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("contacto");
-        if (collision.gameObject.name == "Player") ;
-        {
-            Destroy(gameObject);
+
+        if (collision.gameObject.name == "Player")
+                  {
+                      Destroy(gameObject);
         }
-        
     }
 }
