0f6621fe226ae87db672f8e56ec29a398691fb1e
Ramiro08ds <Sanchezriverazaied@gmail.com>
2025-05-30 10:34:09 -0300

    30/05
    Hay error en el codigo moneymanager y no se cual es, falta la ui que hizo gero al final de la clase

---
+++ Assets/MercanciaScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public float cost;
+    public MoneyManager Moneymanager;
+
+    void Start()
+    {
+        Moneymanager = FindObjectOfType<MoneyManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+
+        }
+    }
+}
